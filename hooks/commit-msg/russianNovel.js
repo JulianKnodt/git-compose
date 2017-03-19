7ea85065f8616e3434f53baa017cff66ae5219bc
julianknodt <julianknodt@gmail.com>
2017-03-19 14:07:31 -0700

    Testing russian novel hook

--- hooks/commit-msg/russianNovel.js
+++ hooks/commit-msg/russianNovel.js
@@ -1,6 +1,7 @@
 const reporter = require('../../utils/report.js');
 const fs = require('fs');
 const runTest = commitMsg => {
+  console.log(commitMsg.toString().split('\n'));
   if(commitMsg.toString().split('\n').length > 10) {
     reporter.warn('Looks like you wrote a russian novel...');
   }
