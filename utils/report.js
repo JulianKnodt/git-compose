6e4607b5a52f4db429572efd56cec3b402d457d0
julianknodt <julianknodt@gmail.com>
2017-03-19 00:39:38 -0700

    Changed color to only invert msg

--- utils/report.js
+++ utils/report.js
@@ -1,14 +1,14 @@
 const chalk = require('chalk');
 module.exports = {
 	success: (msg) => {
-		console.log(chalk.green.inverse('✓', msg.toLowerCase()));
+		console.log(chalk.green('✓'), chalk.green.inverse(msg.toLowerCase()));
 	},
 	error: (msg) => {
-		console.log(chalk.red.inverse('✕', msg.toLowerCase()));
+		console.log(chalk.red('✕'), chalk.red.inverse(msg.toLowerCase()));
 		process.exitCode = 1;
 	},
 	warning: (msg) => {
-		console.log(chalk.yellow.inverse('!', msg.toLowerCase()));
+		console.log(chalk.yellow('!'), chalk.yellow.inverse(msg.toLowerCase()));
 	}
 };
 
