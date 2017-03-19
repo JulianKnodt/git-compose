6e62ce5d6c87289ceb4f6c7f41c5d9581182328c
julianknodt <julianknodt@gmail.com>
2017-03-19 00:38:20 -0700

    Added colors to cli

--- utils/report.js
+++ utils/report.js
@@ -1,12 +1,15 @@
+const chalk = require('chalk');
 module.exports = {
 	success: (msg) => {
-		console.log('✓', msg.toLowerCase());
+		console.log(chalk.green.inverse('✓', msg.toLowerCase()));
 	},
 	error: (msg) => {
-		console.log('✕', msg.toLowerCase());
+		console.log(chalk.red.inverse('✕', msg.toLowerCase()));
 		process.exitCode = 1;
 	},
 	warning: (msg) => {
-		console.log('!', msg.toLowerCase());
+		console.log(chalk.yellow.inverse('!', msg.toLowerCase()));
 	}
 };
+
+console.log(chalk);
